db852fd6f47ddc15fd395366eb619b6db333855d
cmorris <cmorris@CMORRIS-M6600.LSIBartlett.local>
2014-09-27 22:37:19 -0500

    Created project and implemented distance conversion

---
+++ UnitConvertor/src/com/example/unitconvertor/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.unitconvertor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+	public void handleClick(View view){
+		boolean checked = ((RadioButton) view).isChecked();
+		
+		EditText txt = (EditText) findViewById(R.id.text1);
+		double distance = Double.parseDouble(txt.getText().toString());
+		
+		switch(view.getId()){
+		case R.id.radio0:
+			if(checked)
+				txt.setText(kmToMiles(distance));
+			break;
+		case R.id.radio1:
+			if(checked)
+				txt.setText(milesToKm(distance));
+			break;
+			
+		}
+	}
+	
+	public String kmToMiles(double km){
+		double miles = km / 1.609;
+		return String.valueOf(miles);
+	}
+	
+	public String milesToKm(double miles){
+		double km = miles * 1.609;
+		return String.valueOf(km);
+	}
+	
+}
